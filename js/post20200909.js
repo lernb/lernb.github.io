c4a229ae17fddddb510148fe1527036d230b2963
lernb <liuenrg@foxmail.com>
2020-12-04 19:24:25 +0800

    Site updated: 2020-12-04 19:24:25

--- js/post20200909.js
+++ js/post20200909.js
@@ -6,7 +6,7 @@ for (let i = 0; i < pre.length; i++) {
 for (let i = 0; i < h2.length; i++) {
   let h2Pa = document.createElement('div');
   h2Pa.setAttribute('class', 't-h');
-  // h2Pa.setAttribute('style', 'margin-bottom: 23px; border-bottom: 1px solid #eaeaea;');
+  h2Pa.setAttribute('style', 'margin-bottom: 23px; border-bottom: 1px solid #eaeaea;');
   h2[i].parentNode.insertBefore(h2Pa, h2[i].nextElementSibling);
   h2Pa.appendChild(h2[i]);
 }
