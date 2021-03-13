8691c8837a58ea8c14e7d18cfec605e0bd212c67
lernb <liuenrg@foxmail.com>
2021-03-13 22:59:21 +0800

    Site updated: 2021-03-13 22:59:21

--- js/layouts@20200921.js
+++ js/layouts@20200921.js
@@ -1,20 +1,22 @@
-let bar = document.getElementsByClassName('bar-in')[0],
-  pgtimer;
-document.addEventListener('readystatechange', function () {
-  if (document.readyState == "interactive") {
-    let w = 20;
-    bar.style.width = 20 + '%';
-    pgtimer = setInterval(function () {
-      w += .1;
-      if (w <= 95) {
-        bar.style.width = w + '%';
-      }
-    }, 50);
-  } else {
-    bar.style.width = 100 + '%';
-    setTimeout(function () {
-      bar.parentElement.style.display = 'none';
-    }, 300);
-    clearInterval(pgtimer);
-  }
-});
+if (screen.width < 768) {
+  let bar = document.getElementsByClassName('bar-in')[0],
+    pgtimer;
+  document.addEventListener('readystatechange', function () {
+    if (document.readyState == "interactive") {
+      let w = 20;
+      bar.style.width = 20 + '%';
+      pgtimer = setInterval(function () {
+        w += .1;
+        if (w <= 95) {
+          bar.style.width = w + '%';
+        }
+      }, 50);
+    } else {
+      bar.style.width = 100 + '%';
+      setTimeout(function () {
+        bar.parentElement.style.display = 'none';
+      }, 300);
+      clearInterval(pgtimer);
+    }
+  });
+}
