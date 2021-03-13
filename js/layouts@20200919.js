e8b80061b6efeb0732ef36f53dc8e73ddd0a4599
lernb <liuenrg@foxmail.com>
2021-03-13 22:30:30 +0800

    Site updated: 2021-03-13 22:30:29

--- js/layouts@20200919.js
+++ js/layouts@20200919.js
@@ -1,16 +1,15 @@
 let bar = document.getElementsByClassName('bar-in')[0],
   pgtimer;
-  console.log(bar);
 document.addEventListener('readystatechange', function () {
   if (document.readyState == "interactive") {
-    let w = 60;
-    bar.style.width = 60 + '%';
+    let w = 30;
+    bar.style.width = 30 + '%';
     pgtimer = setInterval(function () {
       w += .1;
       if (w <= 95) {
         bar.style.width = w + '%';
       }
-    }, 100);
+    }, 50);
   } else {
     bar.style.width = 100 + '%';
     setTimeout(function () {
